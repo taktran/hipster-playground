51f30eddf20c6cc528395c7966e81d31627379a7
Tak Tran <contact@tutaktran.com>
2014-01-09 00:50:20 +0000

    Attempt at getting gulp-react working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
+
 var lr = require('tiny-lr');
 var refresh = require('gulp-livereload');
 var server = lr();
+
+var react = require('gulp-react');
 
 var config =  {
   livereloadPort: 35729
@@ -12,9 +15,19 @@ gulp.task('livereload', function () {
     .pipe(refresh(server));
 });
 
+gulp.task('jsx', function () {
+  gulp.src('views/templates/CommentBox.jsx')
+    .pipe(react())
+    .pipe(gulp.dest('public/js'));
+});
+
 gulp.task('default', function(){
   server.listen(config.livereloadPort, function (err) {
     if (err) return console.log(err);
+
+    gulp.watch('views/templates/*.jsx', function(event) {
+      gulp.run('jsx');
+    });
 
     gulp.watch([
       'public/js/**',
